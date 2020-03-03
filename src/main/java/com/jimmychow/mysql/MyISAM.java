9a4e824b09efea42dfa111366e25166120d72a54
ichbinzz <zhenming_zhou@hotmail.com>
2020-03-03 12:37:41 +0800

    Fix typo

--- src/main/java/com/jimmychow/mysql/MyISAM.java
+++ src/main/java/com/jimmychow/mysql/MyISAM.java
@@ -2,7 +2,7 @@ package com.jimmychow.mysql;
 
 public interface MyISAM {
     void create(String A, Integer B, String C, Integer D);
-    void retrieve(Integer B);
+    void retrieve(Integer D);
     void retrieveByIndex(Integer B);
     Integer getAll();
     void deleteAll();
